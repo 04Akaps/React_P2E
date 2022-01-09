974bb833d292af5e1b1757b9c9c607fae4e6cf91
유호진 <83758624+jjimgo@users.noreply.github.com>
2022-01-09 09:36:50 +0900

    Delete Client directory

--- Client/src/components/NFTList/NFTList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./NFTList.css";
-function NFTList() {
-  return <div className="NFTList">sdflksdf</div>;
-}
-
-export default NFTList;
--- Client/src/utils/function/openLogin.js
+++
@@ -1,5 +0,0 @@
-export const VERIFIER = {
-  loginProvider: "google",
-  clientId:
-    "BOQHbFBwvHswh7_BlULlJsSpv4H-K7bCFq3TWolp87PjF_kDdHwFktu4E520z6Co86XvGkrZ-9g_U9naNPApgB8",
-};
